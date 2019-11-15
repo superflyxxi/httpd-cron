31efdb58b072415d77578ea9628a49dd49b2df0e
SuperFlyXXI <superflyxxi@yahoo.com>
2019-11-15 11:30:31 -0500

    Try files instead of directories

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,6 +1,6 @@
 touch cron.log
 touch minute.log
-docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`/cron.log:/var/log/cron.log" -v "`pwd`:/home/test" httpd-cron:build
+docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`/cron.log:/var/log/cron.log" -v "`pwd`/minute.log:/home/test/minute.log" httpd-cron:build
 echo "Sleeping for 2 minutes"
 sleep 2m
 docker logs test
