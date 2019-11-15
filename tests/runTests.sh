866591993a13e69156a33de939f421f1ce46cf7c
SuperFlyXXI <superflyxxi@yahoo.com>
2019-11-15 11:04:31 -0500

    Print logs

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,5 +1,6 @@
 docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`:/home/test" httpd-cron:build
 sleep 2m
+docker logs test
 docker stop test
 ls -lha ./
 cat ./minute.log
