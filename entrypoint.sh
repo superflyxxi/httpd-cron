8af28fd3ba3252ce32754ee7e70eeb53454280f0
SuperFlyXXI <superflyxxi@yahoo.com>
2019-11-14 21:35:48 -0500

    Point to /var/log/cron.log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Starting cron server"
-crond -s /var/spool/cron/crontabs -b -L /dev/stdout
+crond -s /var/spool/cron/crontabs -b -L /var/log/cron.log
 
 echo "Starting httpd server"
 httpd-foreground
