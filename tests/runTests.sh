9db5003915f74f6cfe92a39ebd5e4e7cb0071d7e
SuperFlyXXI <superflyxxi@yahoo.com>
2019-11-15 11:05:09 -0500

    Extra logs

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,6 +1,7 @@
-docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`:/home/test" httpd-cron:build
+docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`/cron.log:/var/log/cron.log" -v "`pwd`:/home/test" httpd-cron:build
 sleep 2m
 docker logs test
 docker stop test
 ls -lha ./
+cat ./cron.log
 cat ./minute.log
