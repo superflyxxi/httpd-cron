c213a5c73d23b2b2bd1e5d1f6032037f9017c8dd
SuperFlyXXI <superflyxxi@yahoo.com>
2019-11-15 11:48:37 -0500

    More stuff

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,10 +1,18 @@
 touch cron.log
 touch minute.log
+echo "Running docker"
 docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`/cron.log:/var/log/cron.log" -v "`pwd`/minute.log:/home/test/minute.log" httpd-cron:build
 echo "Sleeping for 2 minutes"
 sleep 2m
+echo "View docker logs"
 docker logs test
 docker stop test
-ls -lha ./
+
+echo "Viewing cron logs"
 cat ./cron.log
+
+echo "Validating test"
+echo "Actual output"
 cat ./minute.log
+echo "Number of lines"
+wc -l < ./minute.log
