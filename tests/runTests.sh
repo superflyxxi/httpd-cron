61c2742c46419a1ae34373ecdd0e35a3ffff8f7b
SuperFlyXXI <superflyxxi@yahoo.com>
2019-11-15 13:25:12 -0500

    Owner should be root

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,5 +1,6 @@
 touch cron.log
 touch minute.log
+chown root:root crontab
 echo "Running docker"
 docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`/cron.log:/var/log/cron.log" -v "`pwd`/minute.log:/home/test/minute.log" httpd-cron:build
 echo "Sleeping for 2 minutes"
@@ -18,3 +19,4 @@ echo "Actual output"
 cat ./minute.log
 echo "Number of lines"
 wc -l < ./minute.log
+
