35b11425a52ed525b033a1e1f0c62f3e17f3a25c
SuperFlyXXI <superflyxxi@yahoo.com>
2019-11-15 13:31:10 -0500

    Chown the file outside of script

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,6 +1,5 @@
 touch cron.log
 touch minute.log
-chown root:root crontab
 echo "Running docker"
 docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`/cron.log:/var/log/cron.log" -v "`pwd`/minute.log:/home/test/minute.log" httpd-cron:build
 echo "Sleeping for 2 minutes"
