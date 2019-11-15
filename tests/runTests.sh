a5dfff6fa79297292bc151e82c9a82b5a9a96a98
SuperFlyXXI <superflyxxi@yahoo.com>
2019-11-15 11:22:35 -0500

    Comment about sleep

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,6 +1,7 @@
 touch cron.log
 touch minute.log
 docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`/cron.log:/var/log/cron.log" -v "`pwd`:/home/test" httpd-cron:build
+echo "Sleeping for 2 minutes"
 sleep 2m
 docker logs test
 docker stop test
