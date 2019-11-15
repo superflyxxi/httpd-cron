599d3c1fe6b46786026037b88719581dffeef691
SuperFlyXXI <superflyxxi@yahoo.com>
2019-11-15 13:37:03 -0500

    Do actual compare

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -3,9 +3,9 @@ touch minute.log
 chown root:root crontab
 echo "Running docker"
 docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`/cron.log:/var/log/cron.log" -v "`pwd`/minute.log:/home/test/minute.log" httpd-cron:build
-echo "Sleeping for 2 minutes"
+echo "Sleeping for 1 minute(s)"
 date
-sleep 2m
+sleep 1m
 date
 echo "View docker logs"
 docker logs test
@@ -18,5 +18,11 @@ echo "Validating test"
 echo "Actual output"
 cat ./minute.log
 echo "Number of lines"
-wc -l < ./minute.log
+LINES=$(wc -l < ./minute.log)
+if [[ ${LINES} -eq 1 ]]; then
+	echo "PASS: Perfection (${LINES})"
+else
+	echo "FAIL: Expected 1 line, but got ${LINES}"
+	exit 1
+fi
 
