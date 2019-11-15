bcbff5405df267ac1e1cc408e3f84db621d79e8f
SuperFlyXXI <superflyxxi@yahoo.com>
2019-11-15 11:58:02 -0500

    Show time before and after sleep

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -3,7 +3,9 @@ touch minute.log
 echo "Running docker"
 docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`/cron.log:/var/log/cron.log" -v "`pwd`/minute.log:/home/test/minute.log" httpd-cron:build
 echo "Sleeping for 2 minutes"
+date
 sleep 2m
+date
 echo "View docker logs"
 docker logs test
 docker stop test
