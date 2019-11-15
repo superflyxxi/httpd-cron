5682135d7a3f4de1a50f67acc276b66fea036f74
SuperFlyXXI <superflyxxi@yahoo.com>
2019-11-15 11:19:44 -0500

    Touch some files

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,3 +1,5 @@
+touch cron.log
+touch minute.log
 docker run --rm -d --name test -v "`pwd`/crontab:/etc/crontabs/root:ro" -v "`pwd`/cron.log:/var/log/cron.log" -v "`pwd`:/home/test" httpd-cron:build
 sleep 2m
 docker logs test
